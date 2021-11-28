d2186642441a74d9f320dc9f22fea568f9eec1d3
Johannes Landgraf <jo.landgraf@outlook.com>
2021-11-28 23:28:24 +0100

    working on header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import React, {useState} from 'react';
+import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
 
+const Menu = () => (
+    <>
+    <p> <a href="#home"> Home </a></p>
+    <p> <a href="#web3"> What is Web3? </a></p>
+    <p> <a href="#possibility"> Open AI </a></p>
+    <p> <a href="#features"> CaseStudies </a></p>
+    <p> <a href="#blog"> Library </a></p>
+    </>
+)
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <div className="web3__navbar">
             <div className="web3__navbar-links">
@@ -12,15 +23,32 @@ const Navbar = () => {
                     <img src={logo} alt="logo"/>
                  </div>
                  <div className="web3__navbar-links_container">
-                    <p> <a href="#home"> Home </a></p>
-                    <p> <a href="#web3"> What is Web3? </a></p>
-                    <p> <a href="#possibility"> Open AI </a></p>
-                    <p> <a href="#features"> CaseStudies </a></p>
-                    <p> <a href="#blog"> Library </a></p>
+                    <Menu />
                  </div>
             </div>
-            Navbar
+            <div className="web3__navbar-sign">
+                <p> Sign in </p>
+                <button type="button">Sign up</button>
+            </div>
+            <div className="web3__navbar-menu">
+                {toggleMenu
+                    ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+                    : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
+                }
+                {toggleMenu && (
+                    <div className="web3__navbar-menu_container scale-up-center"> 
+                        <div className="web3__navbar-menu_container-links">
+                            <Menu />
+                            <div className="web3__navbar-menu_container-links-sign">
+                                <p> Sign in </p>
+                                <button type="button">Sign up</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div> 
         </div>
+
     )
 }
 
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import { RiPsychotherapyLine } from 'react-icons/ri';
+import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="web3__header section__padding" id="home">
+            <div className="web3__header-content">
+                <h1 className="gradient__text">Let's build with Web3</h1>
+                <p>Web3 offers endless possibilities</p>
+
+                <div className="web3__header-content__input">
+                    <input type="email" placeholder="Your email address" />
+                    <button type="button">Get Started</button>
+                </div>
+                <div className="web3__header-content__people">
+                    <img src={people} alt="people"/>
+                    <p>242 people requested access</p>
+                </div>
+
+                <div className="web3__header-image">
+                    <img src={ai} alt="ai" />
+                </div>
+            </div>
         </div>
     )
 }
